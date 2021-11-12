b778a023364ef994de44742cc661333b38a0765d
Isaac Tait <isaac@mountaintopcoding.com>
2021-11-12 10:45:59 -0500

    transition to tailwindcss

--- src/components/layout.js
+++ src/components/layout.js
@@ -21,13 +21,29 @@ const Layout = ({ location, title, children }) => {
   }
 
   return (
-    <div className="global-wrapper" data-is-root-path={isRootPath}>
+    <div className="max-w-8xl mx-auto" data-is-root-path={isRootPath}>
       <header className="global-header">{header}</header>
       <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+      <footer className="bg-green-200 text-grey-600 rounded-lg shadow-lg text-center px-4 py-4 md:text-xs flex items-center justify-between sm:mx-8">
+        © 1978 - {new Date().getFullYear()}, Royal Ridges Retreat
+        <div>
+          <p>
+            Another&nbsp;
+            <a
+              href="https://mountaintopcoding.dev"
+              className="text-blue-400 hover:text-white"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              mountainTopCoding(
+              <span role="img" aria-label="mountain with snow-cap">
+                &#127956;
+              </span>
+              );
+            </a>{' '}
+            project
+          </p>
+        </div>
       </footer>
     </div>
   )
