6ac7d75076e1de0bb25fb6ce6df8731e21c5b30f
Isaac Tait <isaac@mountaintopcoding.com>
2021-11-13 10:57:43 -0500

    okay breaking out the big guns now...

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,43 +1,10 @@
 import * as React from "react"
-import { graphql } from "gatsby"
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-
-const BlogPostTemplate = ({ data, location }) => {
-  const post = data.markdownRemark
-  const siteTitle = data.site.siteMetadata?.title || `Title`
+const BlogPostTemplate = () => {
 
   return (
-    <Layout location={location} title={siteTitle}>
-      <Seo
-        title={post.frontmatter.title}
-        description={post.frontmatter.description || post.excerpt}
-      />
-    </Layout>
+    <div></div>
   )
 }
 
 export default BlogPostTemplate
-
-export const pageQuery = graphql`
-  query BlogPostBySlug(
-    $id: String!
-  ) {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    mdx(id: { eq: $id }) {
-      id
-      excerpt(pruneLength: 160)
-      body
-      frontmatter {
-        title
-        date(formatString: "MMMM DD, YYYY")
-        description
-      }
-    }
-  }
-`
