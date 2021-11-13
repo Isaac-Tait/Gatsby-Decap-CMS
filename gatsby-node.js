e98d259e51a58f7f94e694c16f4e2398b9940824
Isaac Tait <isaac@mountaintopcoding.com>
2021-11-13 17:15:23 -0500

    too many errors, going to start from scratch

--- gatsby-node.js
+++ gatsby-node.js
@@ -35,15 +35,6 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     return
   }
 
-    // Create your paginated pages
-    paginate({
-      createPage, // The Gatsby `createPage` function
-      items: result.data.allMdx.nodes, // An array of objects
-      itemsPerPage: 5, // How many items you want per page
-      pathPrefix: '/blog', // Creates pages like `/blog`, `/blog/2`, etc
-      component: path.resolve('...'), // Just like `createPage()`
-    })
-
   const posts = result.data.allMdx.nodes
 
   // Create blog posts pages
@@ -56,7 +47,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
       const nextPostId = index === posts.length - 1 ? null : posts[index + 1].id
 
       createPage({
-        path: post.fields.slug,
+        path: post.node.fields.slug,
         component: blogPost,
         context: {
           id: post.id,
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,12 @@
 import React from "react";
 
 import Header from "../components/header";
-import BlogLanding from "../components/blogPostsLanding"
 import Footer from "../components/footer";
 
 const Blog = () => {
     return (
         <div>
         <Header />
-        <BlogLanding />
         <Footer />
         </div>
     );
