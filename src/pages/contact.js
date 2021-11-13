18d401e0a0a772999b45d2468caf0c672238ccd0
Isaac Tait <isaac@mountaintopcoding.com>
2021-11-13 13:58:49 -0500

    add netlify contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,10 +7,31 @@ const Contact = () => {
     return (
         <div className="bg-gradient-to-r from-yellow-400 via-yellow-200 to-indigo-200">
         <Header />
-        <div className="max-w-6xl mx-auto">
-            <p>content</p>
+        <div className="max-w-6xl mx-auto h-screen flex justify-center">
+            <form name="contact" method="POST" data-netlify="true" className="my-4">
+                <div className="my-4 flex flex-col">
+                    <label>Your Name:</label>
+                    <input type="text" name="name" />  
+                </div>
+                <div className="mb-4 flex flex-col">
+                    <label>Your Email:</label>
+                    <input type="email" name="email" />
+                </div>
+                <div className="mb-4 flex flex-col">
+                    <label>Your Phone:</label>
+                    <input name="phone" />
+                </div>
+                <div className="mb-4 flex flex-col">
+                    <label>Message:</label>
+                    <textarea name="message" />
+                </div>
+
+                <div className="bg-indigo-200 text-gray-700 hover:bg-yellow-200 hover:text-green-200 rounded-2xl w-1/3 mx-auto">
+                    <button type="submit" className="flex mx-auto">Send</button>
+                </div>
+            </form>
         </div>   
-        <Footer />  
+            <Footer />  
         </div>
     );
 }
