811c0e784840e678f41eb7632b79e867671fb7fe
Isaac Tait <isaac@mountaintopcoding.com>
2021-11-13 17:37:13 -0500

    fix blog page background

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,8 +5,11 @@ import Footer from "../components/footer";
 
 const Blog = () => {
     return (
-        <div>
+        <div className="bg-gradient-to-r from-yellow-400 via-red-200 to-indigo-200">
         <Header />
+        <div className="h-screen">
+            <p>Content</p>
+        </div>
         <Footer />
         </div>
     );
