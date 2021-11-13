bb4c65ae7160f66f9d605402b1160701a507f4a2
Isaac Tait <isaac@mountaintopcoding.com>
2021-11-13 13:28:22 -0500

    clean up blog pages

--- src/components/bio.js
+++ src/components/bio.js
@@ -27,20 +27,20 @@ const Bio = () => {
   const author = data.site.siteMetadata?.author
 
   return (
-    <div className="bio">
+    <div className="flex flex-wrap justify-between heropattern-topography-yellow-400">
       <StaticImage
-        className="bio-avatar"
-        layout="fixed"
+        layout="constrained"
         formats={["auto", "webp", "avif"]}
-        src="../images/logo.png"
-        width={50}
-        height={50}
+        src="https://royal-ridges-retreat.s3.us-west-1.amazonaws.com/staff/joshuaTait.jpg"
+        width={75}
+        height={75}
         quality={95}
         alt="Profile picture"
+        className="rounded-2xl ml-2 my-2"
       />
       {author?.name && (
-        <p>
-          Written by <strong>{author.name}</strong> {author?.summary || null}
+        <p className="text-green-200 font-semibold flex flex-wrap justify-center content-center mr-2">
+          Written by <strong>&nbsp;{author.name}&nbsp;</strong> {author?.summary || null}
           {` `}
         </p>
       )}
