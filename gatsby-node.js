c25141724d48a843b9e979f601fbbb7971f1b9a9
Isaac Tait <isaac@mountaintopcoding.com>
2021-11-13 17:16:20 -0500

    remove pagination

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,5 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
-const { paginate } = require('gatsby-awesome-pagination');
 
 exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions
