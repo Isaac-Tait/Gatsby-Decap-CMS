73a8036863ff65217efed9c5752ec8c4e355ca4c
Isaac Tait <isaac@mountaintopcoding.com>
2021-11-17 11:24:46 -0500

    resolve naming clash

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Header from "../components/header";
-import Contact from "../components/contact-form"
+import ContactForm from "../components/contactForm"
 import Footer from "../components/footer";
 
 const Contact = () => {
@@ -9,7 +9,7 @@ const Contact = () => {
         <div className="bg-gradient-to-r from-yellow-400 via-yellow-200 to-indigo-200">
         <Header />
         <div className="max-w-6xl mx-auto h-screen flex justify-center">
-            <Contact />
+            <ContactForm />
         </div>   
             <Footer />  
         </div>
