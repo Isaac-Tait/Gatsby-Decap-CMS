c4da767c433ff6202680088a594b65a363f5518b
Isaac Tait <isaac@mountaintopcoding.com>
2021-12-03 10:30:39 -0500

    fixed blog post list page - wrong scope

--- src/templates/blog-post-list.js
+++ src/templates/blog-post-list.js
@@ -7,7 +7,6 @@ import Pager from "../components/pager";
 import Footer from "../components/footer";
 
 const BlogPosts = ({ data, pageContext }) => {
-const image = getImage(data.allMdx.frontmatter.image)
 
     return (
         <div className="bg-gradient-to-r from-yellow-400 via-red-200 to-indigo-200">
@@ -15,28 +14,32 @@ const image = getImage(data.allMdx.frontmatter.image)
             <div className="h-screen grid place-content-center">
               <div className="max-w-6xl md:mx-auto">
                 {
-                  data.allMdx.nodes.map((node) => (
-                    <div className="flex justify-center align-middle">
-                      <article key={node.id}>
-                      <div className="text-pink-800 underline hover:text-indigo-400 text-4xl mt-2">
-                        <Link to={node.slug}>
-                          {node.frontmatter.title}
-                        </Link>
-                      </div>
-                      <GatsbyImage 
-                          image={image}
-                          alt={data.frontmatter.imageAlt}
-                      />
+                  data.allMdx.nodes.map((node) => {
+                    const {frontmatter} = node
+                    const image = getImage(frontmatter.image)
+                    return (
+                      <div className="flex justify-center align-middle">
+                        <article key={node.id}>
+                        <div className="text-pink-800 underline hover:text-indigo-400 text-4xl mt-2">
+                          <Link to={node.slug}>
+                            {node.frontmatter.title}
+                          </Link>
+                        </div>
+                          <GatsbyImage 
+                              image={image}
+                              alt={frontmatter.imageAlt}
+                              className="w-1/4 mt-2"
+                          />
                         <p className="text-gray-600">Posted: {node.frontmatter.date}</p>
                         <p className="ml-4 italic text-gray-700">{node.excerpt}</p>
                       </article>
                     </div>
-                  ))
+                    )}
+                  )
                 }
                 <div>
               <Pager pageContext={pageContext} />
-              </div>
-                
+              </div>  
               </div>
             </div>
           <Footer />
