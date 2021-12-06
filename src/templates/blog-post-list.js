a40073c860857c1779441082a6c71f786a0009c6
Isaac Tait <isaac@mountaintopcoding.com>
2021-12-06 12:22:05 -0500

    cleanup blog list page

--- src/templates/blog-post-list.js
+++ src/templates/blog-post-list.js
@@ -11,7 +11,7 @@ const BlogPosts = ({ data, pageContext }) => {
     return (
         <div className="bg-gradient-to-r from-yellow-400 via-red-200 to-indigo-200">
           <Header />
-            <div className="h-screen grid place-content-center">
+            <div className="md:h-screen grid place-content-center">
               <div className="max-w-6xl md:mx-auto">
                 {
                   data.allMdx.nodes.map((node) => {
