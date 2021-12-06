b8e381d5e15c9d3116c6127870847f9b668dfa35
Isaac Tait <isaac@mountaintopcoding.com>
2021-12-06 12:20:55 -0500

    cleanup summer camp page

--- src/pages/summercamp.js
+++ src/pages/summercamp.js
@@ -10,34 +10,28 @@ const SummerCampPage = () => {
         <Header />
         
         <div className="max-w-6xl mx-auto">
-          <div className="flex flex-wrap text-green-200 font-bold text-xl justify-center md:text-4xl">
+          <div className="flex flex-wrap text-green-200 font-bold text-xl md:text-4xl justify-center">
             Royal Ridges Summer Camp
           </div>
-  
-          <div className="grid grid-rows-3 h-96 lg:h-screen">
-            {/*Row one*/}
-            <div className="text-base md:font-semibold md:uppercase ">
-              <div className="grid grid-cols-3">
-                <div className="col-span-2"></div>
-                <div className="flex flex-col items-center">
-                  <img
-                    alt="medical_image"
-                    src="https://royal-ridges-retreat.s3.us-west-1.amazonaws.com/summer_camp_page/Royal+Ridges-Horse+Camp-Yacolt-Washington-Summer+Camp-Kids+Camp-Christian+Camp-Canoe.JPG"
-                    className="hidden md:block h-48 w-64 rounded-3xl"
-                  />
-                  <div className="text-gray-600 hover:text-green-200 mx-4">
-                    <Link to={'/medical'}>Medical & Liability Waiver</Link>
-                  </div>
+          <div className="md:flex md:justify-center md:h-screen md:my-auto flex flex-col">
+              {/*Item one*/}
+              <div className="flex flex-col items-center mt-4 mb-4">
+                <img
+                  alt="medical_image"
+                  src="https://royal-ridges-retreat.s3.us-west-1.amazonaws.com/summer_camp_page/Royal+Ridges-Horse+Camp-Yacolt-Washington-Summer+Camp-Kids+Camp-Christian+Camp-Canoe.JPG"
+                  className="w-64 rounded-3xl"
+                />
+                <div className="text-gray-600 hover:text-green-200 mx-4">
+                  <Link to={'/medical'}>Medical & Liability Waiver</Link>
                 </div>
               </div>
-              {/*Row two*/}
-              <div className="grid grid-cols-3">
-                <div className="col-span-1"></div>
-                <div className="flex flex-col items-center">
+              {/*Item two*/}
+              <div>
+                <div className="flex flex-col items-center mt-4 mb-4">
                   <img
                     alt="parents_manual"
                     src="https://royal-ridges-retreat.s3.us-west-1.amazonaws.com/summer_camp_page/Royal+Ridges-Horse+Camp-Yacolt-Washington-Summer+Camp-Kids+Camp-Christian+Camp-campers.JPG"
-                    className="hidden md:block h-48 w-64 rounded-3xl"
+                    className="w-64 rounded-3xl"
                   />
                   <p className="text-gray-600 hover:text-green-200 mx-4">
                     <Link to={'/parentmanual'}>
@@ -46,13 +40,13 @@ const SummerCampPage = () => {
                   </p>
                 </div>
               </div>
-              {/*Row three*/}
-              <div className="grid grid-cols-3">
-                <div className="flex flex-col items-center">
+              {/*Item three*/}
+              <div>
+                <div className="flex flex-col items-center mt-4 mb-4">
                   <img
                     alt="staff_application"
                     src="https://royal-ridges-retreat.s3.us-west-1.amazonaws.com/summer_camp_page/Royal+Ridges-Horse+Camp-Yacolt-Washington-Summer+Camp-Kids+Camp-Christian+Camp-Horses.JPG"
-                    className="hidden md:block h-48 w-64 rounded-3xl"
+                    className="w-64 rounded-3xl"
                   />
                   <p className="text-gray-600 hover:text-green-200 mx-4">
                     <Link to={'/summerstaffapp'}>
@@ -62,8 +56,8 @@ const SummerCampPage = () => {
                 </div>
               </div>
             </div>
-          </div>
         </div>
+          
         <Footer />
       </div>
     )
