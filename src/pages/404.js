dd35db172f2b2e6a6b6f7564ab132aa603a1bc6e
Isaac Tait <isaac@mountaintopcoding.com>
2021-12-09 13:20:43 -0500

    update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -12,18 +12,17 @@ const NotFoundPage = () => {
       <main className="text-center py-16 text-4xl font-light">
         <p>
           The page you are looking for seems to have gotten loose. We have been
-          out all night looking for it, to no avail. We will not give up -
-          however, in the meantime head on over to our blog page, which has a
-          handy search feature... Maybe you will find what you are looking for
+          out all night looking for it, to no avail.
+          In the meantime head on over to the home page. Maybe you will find what you are looking for
           there.
         </p>
         <p className="mt-2">
           And just because we are nice here is a handy link to the&nbsp;
           <Link
-            to={'/blog'}
+            to={'/'}
             className="text-green-200 hover:text-yellow-400 hover:bg-indigo-50 underline"
           >
-            Blog
+            Home Page
           </Link>
           .
         </p>
