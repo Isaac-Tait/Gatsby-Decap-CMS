6b01b65ab5569b257d9c04f2897b03acce2e1a79
Isaac Tait <isaac@mountaintopcoding.com>
2021-12-14 10:02:44 -0500

    please do not break my form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -20,50 +20,45 @@ const Comments = () => {
             >
             <input type="hidden" name="form-name" value="contact" />
                 <div className="my-4 flex flex-col">
-                    <label htmlFor="name">Name
-                        <input
+                    <label htmlFor="name">Name</label>
+                    <input
                             className="border-2 border-yellow-400 w-1/4" 
                             type="text" 
                             name="name" 
                         />
-                    </label>
                 </div>
 
                 <div className="mb-4 flex flex-col">
-                    <label htmlFor="email">Email
-                        <input 
-                            className="border-2 border-yellow-400 w-1/4"
-                            type="email" 
-                            name="email" 
-                            
-                        />
-                    </label>
+                    <label htmlFor="email">Email</label>
+                    <input 
+                        className="border-2 border-yellow-400 w-1/4"
+                        type="email" 
+                        name="email" 
+                    />
                 </div>
 
                 <div className="mb-4 flex flex-col">
-                    <label htmlFor="phone">Phone
-                        <input
-                            className="border-2 border-yellow-400 w-1/4"
-                            type="text" 
-                            name="phone" 
-                        />
-                    </label>
+                    <label htmlFor="phone">Phone</label>
+                    <input
+                        className="border-2 border-yellow-400 w-1/4"
+                        type="text" 
+                        name="phone" 
+                    />
                 </div>
 
                 <div className="mb-4 flex flex-col">
-                    <label htmlFor="message">Message
-                        <textarea
-                            className="overflow-y-auto h-60 border-2 border-yellow-400"            
-                            type="text" 
-                            name="message" 
-                        />
-                    </label>
+                    <label htmlFor="message">Message</label>
+                    <textarea
+                        className="overflow-y-auto h-60 border-2 border-yellow-400"            
+                        type="text" 
+                        name="message" 
+                    />
                 </div>
                 
                 <button type="submit" className="bg-indigo-200 text-gray-700 hover:bg-yellow-200 hover:text-green-200 rounded-2xl w-1/4 flex mx-auto cursor-pointer">
                     <p className="underline flex mx-auto">Send</p>
                 </button>
-                
+
             </form>
         </div>
             <Footer />
