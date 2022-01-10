6910f2fb046fd0838a6d12333af9efa2d0504a12
Isaac Tait <isaac@mountaintopcoding.com>
2022-01-10 10:44:44 -0500

    clean up donations page

--- src/pages/donations.js
+++ src/pages/donations.js
@@ -7,7 +7,7 @@ const Donations = () => {
     return (
         <div className="bg-gradient-to-r from-yellow-400 via-yellow-200 to-indigo-200">
           <Header />
-            <div className="max-w-6xl mx-2 md:mx-auto text-xs md:text-base">
+            <div className="max-w-6xl h-screen overflow-y-scroll mx-auto text-xs md:text-base">
               <p className="flex flex-wrap text-green-200 font-bold text-xl justify-center md:text-4xl">
                 Donations
               </p>
@@ -46,82 +46,53 @@ const Donations = () => {
               <p className="italic">
                 All donations are tax-deductible and greatly appreciated.
               </p>
-            <div className="hidden md:block">
-                <div className="grid grid-cols-4">
-                  <div className="h-24 w-24">
-                    <img
-                      alt="10 dollar donation"
-                      src="https://royal-ridges-retreat.s3.us-west-1.amazonaws.com/10don.png"
-                    />
-                  </div>
-                  <div className="h-24 w-24">
-                    <img
-                      alt="25 dollar donation"
-                      src="https://royal-ridges-retreat.s3.us-west-1.amazonaws.com/25don.png"
-                    />
-                  </div>
-                  <div className="h-24 w-24">
-                    <img
-                      alt="50 dollar donation"
-                      src="https://royal-ridges-retreat.s3.us-west-1.amazonaws.com/50don.png"
-                    />
-                  </div>
-                  <div className="h-24 w-24">
-                    <img
-                      alt="100 dollar donation"
-                      src="https://royal-ridges-retreat.s3.us-west-1.amazonaws.com/100don.png"
-                    />
-                  </div>
-                </div>
-              <div className="grid grid-cols-4 mt-2">
-                <div className="h-24 w-24">
-                  <img
-                    alt="250 dollar donation"
-                    src="https://royal-ridges-retreat.s3.us-west-1.amazonaws.com/250don.png"
-                  />
-                </div>
-                <div className="h-24 w-24">
-                  <img
-                    alt="500 dollar donation"
-                    src="https://royal-ridges-retreat.s3.us-west-1.amazonaws.com/500don.png"
-                  />
-                </div>
-                <div className="h-24 w-24">
-                  <img
-                    alt="1,000 dollar donation"
-                    src="https://royal-ridges-retreat.s3.us-west-1.amazonaws.com/1000don.png"
-                  />
-                </div>
-              </div>
-                <p className="italic mt-4">
-                  Or you can donate any amount at paypal.me/royalridges Make sure to add
-                  a note that it is a donation! If you want to send a donation as check
-                  then make it payable to Royal Ridges and send it to PO Box 3010 Battle
-                  Ground, WA 98604 All donations are tax-deductible and greatly
-                  appreciated.
-                </p>
-              <div className="flex flex-wrap justify-between mt-2">
-                <div>
+                
+              <p className="italic mt-4">
+                Or you can donate any amount at paypal.me/royalridges Make sure to add
+                a note that it is a donation! If you want to send a donation as check
+                then make it payable to Royal Ridges and send it to PO Box 3010 Battle
+                Ground, WA 98604 All donations are tax-deductible and greatly
+                appreciated.
+              </p>
+              
+              <div className='ml-10 mt-4 w-1/2 text-center'>
+                <div className='flex justify-center'>
                   <img
                     alt="Amazon Smile"
                     src="https://royal-ridges-retreat.s3.us-west-1.amazonaws.com/amazonSmile.png"
                     className="w-28 rounded-2xl"
                   />
                 </div>
-                <div>
+                <p className='font-semibold text-xl'>How to sign up for AmazonSmile</p>
+                <p className="ml-1">How to use AmazonSmile on a web browser:</p>
+                <p className="ml-1">Visit smile.amazon.com</p>
+                <p className="ml-1">Sign in with the same account you use for Amazon.com</p>
+                <p className="ml-1">Select your charity</p>
+                <p className="ml-1">Start shopping! Remember to check out at smile.amazon.com to generate donations for your chosen charity.</p>
+                <p className="ml-1">Tip: Add a bookmark to make it easier to shop at smile.amazon.com.</p>
+
+                <p className='font-semibold text-xl'>How to use AmazonSmile using the Amazon app on your mobile phone:</p>
+                <p className='ml-1'>Open the Amazon Shopping app</p>
+                <p className='ml-1'>Navigate to the main menu (=)</p>
+                <p className='ml-1'>Tap on Settings and then select “AmazonSmile”</p>
+                <p className='ml-1'>Select your charity and then follow the on-screen instructions to turn ON AmazonSmile in the mobile app</p>
+                <p className='ml-1'>Once AmazonSmile has been activated in your app, future eligible app purchases will generate a donation for the charity you have selected.</p>
+                <p className='ml-1'>Note: The tablet app is not yet supported. Please visit smile.amazon.com/onthego to learn more.</p>
+
+                <div className='flex justify-center mt-2'>
                   <img
                     alt="Fred Meyers"
                     src="https://royal-ridges-retreat.s3.us-west-1.amazonaws.com/fredMeyers.png"
                     className="w-28 rounded-2xl"
                   />
                 </div>
+                <p className="mt-4">
+                  Designate Royal Ridges Retreat as your favorite charity with your Fred
+                  Meyer Community Rewards. Note: this does not detract from your rewards
+                  earned.
+                </p>
               </div>
-              <p className="mt-4">
-                Designate Royal Ridges Retreat as your favorite charity with your Fred
-                Meyer Community Rewards. Note: this does not detract from your rewards
-                earned.
-              </p>
-            </div>
+  
           </div>
         <Footer />
       </div>
