d817c90bfd7a896c4d8f50933ccb6d632ae19b1e
Isaac Tait <isaac@mountaintopcoding.com>
2022-01-18 19:36:25 -0500

    add eslint/prettier 🤷🏻‍♂️

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line jsx-a11y/rule-name
 import * as React from "react"
 import { Link } from "gatsby"
 
@@ -9,26 +8,26 @@ import Footer from "../components/footer";
 const NotFoundPage = () => {
   return (
     <div>
-    <Header />
-      <main className="text-center py-16 text-4xl font-light">
-        <p>
-          The page you are looking for seems to have gotten loose. We have been
-          out all night looking for it, to no avail.
-          In the meantime head on over to the home page. Maybe you will find what you are looking for
-          there.
-        </p>
-        <p className="mt-2">
-          And just because we are nice here is a handy link to the&nbsp;
-          <Link
-            to={'/'}
-            className="text-green-200 hover:text-yellow-400 hover:bg-indigo-50 underline"
-          >
-            Home Page
-          </Link>
-          .
-        </p>
-      </main>
-      <Footer />
+      <Header />
+        <main className="text-center py-16 text-4xl font-light">
+          <p>
+            The page you are looking for seems to have gotten loose. We have been
+            out all night looking for it, to no avail.
+            In the meantime head on over to the home page. Maybe you will find what you are looking for
+            there.
+          </p>
+          <p className="mt-2">
+            And just because we are nice here is a handy link to the&nbsp;
+            <Link
+              to={'/'}
+              className="text-green-200 hover:text-yellow-400 hover:bg-indigo-50 underline"
+            >
+              Home Page
+            </Link>
+            .
+          </p>
+        </main>
+        <Footer />
     </div>
   );
 }
