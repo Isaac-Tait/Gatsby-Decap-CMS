e2c2916da07812668c0d0b2934cb1ff273a941e6
Isaac Tait <isaac@mountaintopcoding.com>
2022-01-27 17:26:26 -0500

    fix updates page

--- src/components/pager.js
+++ src/components/pager.js
@@ -7,7 +7,7 @@ const Pagination = ({ pageContext }) => {
   const isFirst = currentPage === 1
   const isLast = currentPage === numPages
   const prevPage =
-    currentPage - 1 === 1 ? '/blog' : (currentPage - 1).toString()
+    currentPage - 1 === 1 ? '/updates' : (currentPage - 1).toString()
   const nextPage = (currentPage + 1).toString()
 
   return (
@@ -27,7 +27,7 @@ const Pagination = ({ pageContext }) => {
         {Array.from({ length: numPages }, (_, i) => (
           <div key={`pagination-number${i + 1}`}>
             <Link
-              to={`/blog/${i === 0 ? '' : i + 1}`}
+              to={`/updates/${i === 0 ? '' : i + 1}`}
               className="bg-yellow-400 rounded-xl px-2 text-gray-700 hover:text-indigo-400 hover:bg-white"
             >
               {i + 1}
