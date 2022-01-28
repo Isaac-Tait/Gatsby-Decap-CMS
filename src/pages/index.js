cc35a02ec188faf9230f22b7e6e6ae013ba7ae89
Isaac Tait <isaac@mountaintopcoding.com>
2022-01-28 11:09:38 -0500

    Update tab 🤩

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <Seo title="All posts" />
+      <Seo title="Explore" />
       <HomePage />
     </Layout>
   )
