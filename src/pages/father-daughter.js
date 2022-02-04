78c4c23312aad1aeecf6ef3defe60838ba784bcc
Isaac Tait <isaac@mountaintopcoding.com>
2022-02-04 15:00:55 -0500

    meeting discussion updates 🧑🏼‍🎨

--- src/pages/father-daughter.js
+++ src/pages/father-daughter.js
@@ -9,9 +9,10 @@ const FatherDaughter = () => {
         <div className='bg-gradient-to-r from-yellow-400 via-yellow-200 to-indigo-200'>
             <Header />
                 <div className='h-screen'>
+                <p className="flex flex-wrap text-green-200 font-bold font-cursive text-xl justify-center md:text-4xl">Father Daughter</p>
                     <div className="max-w-6xl h-1/2 mx-auto overflow-y-scroll grid grid-cols-2 gap-4">
                         {/*Column One (Left)*/}
-                        <div className='flex flex-col justify-between bg-indigo-300 mt-2'>
+                        <div className='flex flex-col justify-between mt-2'>
                             <div>
                                 <Link to={'/mother-daughter'}>
                                 <p className="text-green-200 hover:text-indigo-400 font-bold text-lg justify-center md:text-2xl">Mother Daughter</p>
@@ -23,19 +24,14 @@ const FatherDaughter = () => {
                                 </Link>
                             </div>
                             <div>
-                                <Link to={'/mother-daughter'}>
-                                <p className="text-green-200 hover:text-indigo-400 font-bold text-lg justify-center md:text-2xl">Father Daughter</p>
-                                </Link>
-                            </div>
-                            <div>
                                 <Link to={'/father-son'}>
                                 <p className="text-green-200 hover:text-indigo-400 font-bold text-lg justify-center md:text-2xl">Father Son</p>
                                 </Link>
                             </div>
                         </div>
                         {/*Column Two (Right)*/}
-                        <div className='bg-pink-300 mt-2'>
-                            <p>Stuff here</p>
+                        <div className='mt-2'>
+                            <p>Father Daughter content</p>
                         </div>
                     </div>
                 </div>
--- src/pages/mother-daughter.js
+++ src/pages/mother-daughter.js
@@ -9,21 +9,18 @@ const MotherDaughter = () => {
         <div className='bg-gradient-to-r from-yellow-400 via-yellow-200 to-indigo-200'>
             <Header />
                 <div className='h-screen'>
+                <p className="flex flex-wrap text-green-200 font-bold font-cursive text-xl justify-center md:text-4xl">Mother Daughter</p>
                     <div className="max-w-6xl h-1/2 mx-auto overflow-y-scroll grid grid-cols-2 gap-4">
+                    
                         {/*Column One (Left)*/}
-                        <div className='flex flex-col justify-between bg-indigo-300 mt-2'>
-                            <div>
-                                <Link to={'/mother-daughter'}>
-                                <p className="text-green-200 hover:text-indigo-400 font-bold text-lg justify-center md:text-2xl">Mother Daughter</p>
-                                </Link>
-                            </div>
+                        <div className='flex flex-col justify-between mt-2'>
                             <div>
                                 <Link to={'/mother-son'}>
                                 <p className="text-green-200 hover:text-indigo-400 font-bold text-lg justify-center md:text-2xl">Mother Son</p>
                                 </Link>
                             </div>
                             <div>
-                                <Link to={'/mother-daughter'}>
+                                <Link to={'/father-daughter'}>
                                 <p className="text-green-200 hover:text-indigo-400 font-bold text-lg justify-center md:text-2xl">Father Daughter</p>
                                 </Link>
                             </div>
@@ -34,8 +31,8 @@ const MotherDaughter = () => {
                             </div>
                         </div>
                         {/*Column Two (Right)*/}
-                        <div className='bg-pink-300 mt-2'>
-                            <p>Stuff here</p>
+                        <div className='mt-2'>
+                            <p>Mother Daughter Content</p>
                         </div>
                     </div>
                 </div>
