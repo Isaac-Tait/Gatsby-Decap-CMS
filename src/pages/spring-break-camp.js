11e346efd7893898c7494b8143593d810bc3f221
Isaac Tait <isaac@mountaintopcoding.com>
2022-03-10 09:52:52 -0500

    feat: add spring break camp page 😳

---
+++ src/pages/spring-break-camp.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Header from "../components/header";
+import Footer from "../components/footer";
+
+const SpringBreakCamp = () => {
+    return (
+        <div className="bg-gradient-to-t from-yellow-200 to-indigo-200">
+            <Header />
+            <p>Oops migrating sites is not without its little hiccups... Stay tuned this page will be live asap.</p>
+            <Footer />
+        </div>
+    )
+};
+
+export default SpringBreakCamp;
