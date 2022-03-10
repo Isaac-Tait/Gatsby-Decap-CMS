2ead96fbbc0d32f77bd9bf19c3759a80c4b0def5
Isaac Tait <isaac@mountaintopcoding.com>
2022-03-10 13:30:59 -0500

    chore: add missing pages w/ logic 💩

--- src/pages/school-break.js
+++ src/pages/school-break.js
@@ -1,15 +1,33 @@
 import React from "react";
 
+import { Link } from 'gatsby'
+
 import Header from "../components/header";
 import Footer from "../components/footer";
 
 const SchoolBreak = () => {
     return (
-        <div className="bg-gradient-to-r from-yellow-200 to-indigo-200">
+        <div className="bg-gradient-to-r from-yellow-200 to-green-200">
             <Header />
-                <div className="max-w-6xl mx-2 md:mx-auto text-xs md:text-base h-screen overflow-y-scroll">
-                    <img src='https://royal-ridges-retreat.s3.us-west-1.amazonaws.com/programs-page/headerPhotos/SchoolBreakCamps.png' alt='vaulting class' className='w-full rounded-2xl shadow-xl mt-2' />
-                    <p>Content</p>
+            <div className='h-screen grid grid-cols-2 max-w-6xl mx-auto'>
+                    {/*Column 1*/}
+                    <div className='flex flex-col mx-auto'>
+                        <Link to={'/spring-day-camp'} className='font-cursive my-4 hover:underline text-green-200 hover:text-indigo-500'>
+                            <p>Spring Day Camp</p>
+                        </Link>
+                        <Link to={'/winter-day-camp'} className='font-cursive my-4 hover:underline text-green-200 hover:text-indigo-500'>
+                            <p>Winter Day Camp</p>
+                        </Link>
+                    </div>
+                    {/*Column 2*/}
+                    <div className='mr-4 overflow-y-scroll'>
+                        <img 
+                            src='https://royal-ridges-retreat.s3.us-west-1.amazonaws.com/programs-page/headerPhotos/SchoolBreakCamps.png' 
+                            alt='summer camp descriptions'
+                            className='rounded-xl mt-1'
+                        />
+                        <p>Oops migrating sites is not without its little hiccups... Stay tuned this page will be live asap. (3/10/22)</p>
+                    </div>    
                 </div>
             <Footer />
         </div>
--- src/pages/winter-day-camp.js
+++ src/pages/winter-day-camp.js
@@ -5,26 +5,26 @@ import { Link } from 'gatsby'
 import Header from "../components/header";
 import Footer from "../components/footer";
 
-const SpringDayCamp = () => {
+const WinterDayCamp = () => {
     return (
-        <div className="bg-gradient-to-b from-green-200 to-yellow-200">
+        <div className="bg-gradient-to-b from-yellow-200 to-green-200">
             <Header />
-            <div className='h-screen grid grid-cols-2 max-w-6xl mx-auto'>
+                <div className='h-screen grid grid-cols-2 max-w-6xl mx-auto'>
                     {/*Column 1*/}
                     <div className='flex flex-col mx-auto'>
                         <Link to={'/school-break'} className='mx-auto my-4 font-cursive hover:underline font-extrabold text-xl text-green-200 hover:text-indigo-500'>
                             <p>School Break Camps</p>
                         </Link>
                         <hr />
-                        <Link to={'/winter-day-camp'} className='font-cursive my-4 hover:underline text-green-200 hover:text-indigo-500'>
-                            <p>Winter Day Camp</p>
+                        <Link to={'/spring-day-camp'} className='font-cursive my-4 hover:underline text-green-200 hover:text-indigo-500'>
+                            <p>Spring Day Camp</p>
                         </Link>
                     </div>
                     {/*Column 2*/}
                     <div className='mr-4 overflow-y-scroll'>
                         <img 
                             src='https://royal-ridges-retreat.s3.us-west-1.amazonaws.com/programs-page/headerPhotos/SchoolBreakCamps.png' 
-                            alt='spring camp'
+                            alt='summer camp descriptions'
                             className='rounded-xl mt-1'
                         />
                         <p>Oops migrating sites is not without its little hiccups... Stay tuned this page will be live asap. (3/10/22)</p>
@@ -35,4 +35,4 @@ const SpringDayCamp = () => {
     )
 };
 
-export default SpringDayCamp;
+export default WinterDayCamp;
