bc1495c402f2ca06ec3fb46afa54fad3a66230a3
Isaac Tait <isaac@mountaintopcoding.com>
2022-03-21 16:50:24 -0400

    feat: clarify site nav && add new icons 🧚🏼‍♀️

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -94,20 +94,21 @@ const HomePage = () => {
                 width={600}
               />
             </div>
-            <div className="w-1/2 flex justify-center mx-auto mb-10">
-            <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6 animate-bounce"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
+            <div className="flex justify-center mb-10">
+              <p className='bg-white px-1 text-gray-600'>Scroll Down</p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 animate-bounce text-green-200"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
                 >
-                    <path
+                  <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth={2}
                     d="M19 14l-7 7m0 0l-7-7m7 7V3"
-                    />
+                  />
                 </svg>
             </div>
         </div>
