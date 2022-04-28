61edd90479a4e66548be74c90b5932185d414eed
Isaac Tait <isaac@mountaintopcoding.com>
2022-04-28 18:26:31 -0400

    chore: check summer camp availability 🌵

--- src/pages/account.js
+++ src/pages/account.js
@@ -14,7 +14,8 @@ const Account = () => {
                 <a 
                   href='https://www.ultracamp.com/clientlogin.aspx?idCamp=1145&campCode=151'
                   className='text-green-200 hover:text-indigo-400 hover:underline'
-                  target="_blank" rel="noopener noreferrer"
+                  target="_blank" 
+                  rel="noopener noreferrer"
                 >here</a>.</p>
             </div>
         <Footer />
