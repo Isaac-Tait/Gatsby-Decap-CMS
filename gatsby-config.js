d907c72384ad96223df414d4a8cd33e293cdba3b
Isaac Tait <isaac@mountaintopcoding.com>
2022-09-28 20:46:24 -0400

    chore: update book a ride page 🐴

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,6 +76,10 @@ module.exports = {
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {},
+    },
     `gatsby-plugin-react-helmet`,
   ],
 }
--- src/pages/book-a-ride.js
+++ src/pages/book-a-ride.js
@@ -20,35 +20,29 @@ const BookARide = () => {
                   />
                 </div>
               <p className="mt-2">
-                We offer trail rides, for guests 9 years and older (no age exceptions
-                allowed).
+              Royal Ridges offers trail rides for guests nine years and older ( Sorry, no age exceptions allowed).
               </p>
               <p className="mt-2">
-                Arena rides are also available for ages 6 and up. Your group must
-                choose to do an arena ride OR a trail ride, we cannot accommodate both
-                simultaneously for this program.
+              Arena rides are also available for ages six and up. Your group must choose to do an arena ride OR a trail ride as we cannot accommodate both simultaneously for this program.
               </p>
-              <p className="mt-2">
-                Please arrive at your scheduled ride time and expect to be riding
-                about 45 minutes and be there a total of an hour to an hour and a
-                half. Your group may help with unsaddling and grooming at the
-                discretion of the wrangler.
-              </p>
-              <p className="mt-2">
-                The minimum package is $225 for up to 5 people. The fee for additional
-                riders in the group is $45/each. Each trail ride can have up to 8-10
-                riders and arena ride can have up to 6 riders. Please let us know if
-                riders will be under 9 years old when booking.
-              </p>
-              <p className="mt-2">
-                If you need to cancel, it must be done by noon the day prior to your
-                ride with only a 50% refund of the total fee.
-              </p>
-              <p className="mt-2">
-                Weight limit is up to 250 lbs and no expectant mothers are to ride for
-                safety reasons. Please honor our weight limit requirement as our
-                horses are often older and mostly used for youth and child programs.
-              </p>
+              <p className="text-center">Interested? Please contact <a href="mailto:programs@royalridges.org">programs@royalridges.org</a></p>
+              <li>
+                <ul className="mt-2">
+                Please arrive 15 minutes before your scheduled time.
+                </ul>
+                <ul className="mt-2">
+                The riding time is approximately 45 minutes; plan for your total Book-A-Ride experience to be 1 hour to 1-½ hours long. Your group may help with unsaddling and grooming at the wrangler's discretion.
+                </ul>
+                <ul className="mt-2">
+                All riders must have their liability form filled out. Please be sure to complete this before you arrive as this can cause your time to be wasted. Use this link: <a href="https://waiver.smartwaiver.com/w/57d1b3426821c/web/" target="_blank" rel="noopener noreferrer">Liability Form</a>
+                </ul>
+                <ul className="mt-2">
+                The weight limit is up to 250 lbs,  and no expectant mothers may ride for safety reasons. Please honor our weight limit requirement as our horses are older and primarily used for youth and children programs.
+                </ul>
+                <ul>
+                If you need to cancel, it must be done by noon the day before your ride. We will return all but 50% of the total fee.
+                </ul>
+              </li>
               <p className="mt-2">
                 If you have any questions or want more info please{' '}
                 <a
