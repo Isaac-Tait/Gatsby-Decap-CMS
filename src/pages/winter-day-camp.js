00aa22e5a14adf49d0b5a6958ce52aa3d6d6f350
Joshua <joshua@royalridges.org>
2022-10-26 15:52:48 -0700

    Updated Link

--- src/pages/winter-day-camp.js
+++ src/pages/winter-day-camp.js
@@ -27,7 +27,31 @@ const WinterDayCamp = () => {
                             alt='summer camp descriptions'
                             className='rounded-xl mt-1'
                         />
-                        <p>Stay tuned in the Fall of 2022 when we will release the dates for Winter Break 2022/23!</p>
+                         <p className='mb-2 text-center'>Five action-packed days of Winter Camp fun and activities!</p>
+                        <p>Join us this Christmas break for our Winter Day Camp as we create 5 days of fun-filled holiday-themed adventures for campers from Kindergarten through 6th grade. We are looking forward to filling camp with the joy of the season.  We also offer a break-out horse activity for those kiddos who love horses. Space is limited, so click on the link below for more information and to register for camp.</p>
+                        <p className='mb-2'>Choose either Traditional Day Camp or Day Horse Camp - either way, your child is sure to have a great time!</p>
+                        <hr />
+                        <p className='my-2'>Checkout the updates page for more info (including bus schedules)
+                            <a 
+                                href='https://royalridges.org/updates/winter-break-camp/'
+                                target="_blank" 
+                                rel="noopener noreferrer" 
+                                className='text-green-200 hover:text-indigo-400 hover:underline'
+                            >&nbsp;here</a>.</p>
+                        <hr />
+                        <p className='mt-1'><span className='font-bold'>Date: </span>April 4-8, 2022</p>
+                        <p><span className='font-bold'>Time: </span>8:30am - 3:30pm</p>
+                        <p><span className='font-bold'>Ages: </span>Kindergarten to 6th Grade</p>
+                        <p className='font-bold'>Prices:</p>
+                        <p className='ml-1'>Traditional Day Camp: $175.00 per child</p>
+                        <p className='ml-1'>Horse Option: $150 per child</p>
+                        <p className='mt-2 text-center font-cursive text-xl'>Register 
+                        <a 
+                            href='https://www.ultracamp.com/info/upcomingSessions.aspx?idCamp=1145&campCode=151&lnkCategory=Spring+Break+Camp' 
+                            target="_blank" 
+                            rel="noopener noreferrer" 
+                            className='text-green-200 hover:text-indigo-400 hover:underline'
+                        >&nbsp;here</a>.</p>
                     </div>    
                 </div>
             <Footer />
