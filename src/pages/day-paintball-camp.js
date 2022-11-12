1abc531ba00cb374838565eab1957ad35d2c91d7
Isaac Tait <isaac@mountaintopcoding.com>
2022-11-12 10:41:09 -0500

    chore: update paintball camp page 👼🏽

--- src/pages/day-paintball-camp.js
+++ src/pages/day-paintball-camp.js
@@ -54,26 +54,26 @@ const DayPaintBall = () => {
                         >Register for Paintball Camp</a>
                         <div className='font-semibold text-green-200 ml-4 my-4 text-center'>
                             <p>Monday-Friday 9am-3:30pm</p>        
-                            <p>$315 for the week</p>             
-                            <p>$50 Bus Transportation</p>
+                            <p>$350 for the week</p>             
+                            <p>$60 Bus Transportation</p>
                             <p>SPLAT Entering Grades 3rd - 6th</p> 
                             <p>EXTREME Entering Grades 6th - 10th</p> 
                         </div>
                         <hr />
-                        <p><span className='text-green-200 font-semibold'>Paintball Day Camp</span>&nbsp;~&nbsp;Campers learn and play the extreme sport of Paintball.  In the morning, campers learn the finer points of the game while fine-tuning their skills. Team building activities help teach communication skills while we grow our character through a daily Bible Study. The last three hours of the day are game-time, where it is time to sling paint!</p>
+                        <p><span className='text-green-200 font-semibold'>Paintball Day Camp</span>&nbsp;~&nbsp;Paintball Day Camp ~ Campers learn and play the extreme sport of Paintball. In the morning, campers learn the game's finer points while fine-tuning their skills. Team building activities help teach communication skills while we grow our character through daily Bible Study. The last three hours of the day are game time, where it is time to sling paint!</p>
                         <p className='italic mt-4'>Extreme uses conventional markers and balls at 290 fps, with woodsball being the primary emphasis.</p>
                         <p className='italic my-4'>Splat is our low-impact paintball which uses pump markers, a smaller ball, max. speed at 130 fps and is played mainly in the Tactical Village.</p>
                         <hr />
                         <div className='my-2 ml-2 text-center'>
-                            <p><span className='text-green-200 font-semibold'>Week #1</span> - June 20-24<span className='text-green-200 font-bold'>&nbsp;SPLAT</span></p>
-                            <p><span className='text-green-200 font-semibold'>Week #2</span> - June 27-July 1<span className='text-green-200 font-bold'>&nbsp;SPLAT</span></p>  
-                            <p><span className='text-green-200 font-semibold'>Week #3</span> - July 4-8<span className='text-yellow-400 font-bold'>&nbsp;EXTREME</span></p>              
-                            <p><span className='text-green-200 font-semibold'>Week #4</span> - July 11-15<span className='text-green-200 font-bold'>&nbsp;SPLAT</span></p>           
-                            <p><span className='text-green-200 font-semibold'>Week #5</span> - July 18-22<span className='text-green-200 font-bold'>&nbsp;SPLAT</span></p>           
-                            <p><span className='text-green-200 font-semibold'>Week #6</span> - July 25-29<span className='text-yellow-400 font-bold'>&nbsp;EXTREME</span></p>
-                            <p><span className='text-green-200 font-semibold'>Week #7</span> - August 1-5<span className='text-green-200 font-bold'>&nbsp;SPLAT</span></p>
-                            <p><span className='text-green-200 font-semibold'>Week #8</span> - August 8-12<span className='text-green-200 font-bold'>&nbsp;SPLAT</span></p>
-                            <p><span className='text-green-200 font-semibold'>Week #9</span> - August 15-19<span className='text-yellow-400 font-bold'>&nbsp;EXTREME</span></p>
+                            <p><span className='text-green-200 font-semibold'>Week #1</span> - June 19-23<span className='text-green-200 font-bold'>&nbsp;SPLAT</span></p>
+                            <p><span className='text-green-200 font-semibold'>Week #2</span> - June 26-30<span className='text-green-200 font-bold'>&nbsp;SPLAT</span></p>  
+                            <p><span className='text-green-200 font-semibold'>Week #3</span> - July 3-7<span className='text-yellow-400 font-bold'>&nbsp;EXTREME</span></p>              
+                            <p><span className='text-green-200 font-semibold'>Week #4</span> - July 10-14<span className='text-green-200 font-bold'>&nbsp;SPLAT</span></p>           
+                            <p><span className='text-green-200 font-semibold'>Week #5</span> - July 17-21<span className='text-green-200 font-bold'>&nbsp;SPLAT</span></p>           
+                            <p><span className='text-green-200 font-semibold'>Week #6</span> - July 24-28<span className='text-yellow-400 font-bold'>&nbsp;EXTREME</span></p>
+                            <p><span className='text-green-200 font-semibold'>Week #7</span> - July 31 - August 4<span className='text-green-200 font-bold'>&nbsp;SPLAT</span></p>
+                            <p><span className='text-green-200 font-semibold'>Week #8</span> - August 7-11<span className='text-green-200 font-bold'>&nbsp;SPLAT</span></p>
+                            <p><span className='text-green-200 font-semibold'>Week #9</span> - August 14-18<span className='text-yellow-400 font-bold'>&nbsp;EXTREME</span></p>
                         </div>
                         <a 
                             href='https://www.ultracamp.com/clientlogin.aspx?idCamp=1145&campCode=151' 
