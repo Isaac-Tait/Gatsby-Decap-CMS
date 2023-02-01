59bddbc09d9ccf61d32e9217b955da794f93c162
Isaac Tait <isaac@mountaintopcoding.com>
2023-02-01 12:07:34 -0500

    fix: update blog post lists 😎

--- src/templates/blog-post-list.js
+++ src/templates/blog-post-list.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import { graphql, Link } from 'gatsby'
-import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import React from 'react';
+import { graphql, Link } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
-import Header from '../components/header'
-import Pagination from '../components/pager'
-import Footer from '../components/footer'
+import Header from '../components/header';
+import Pagination from '../components/pager';
+import Footer from '../components/footer';
 
 const BlogPosts = ({ data, pageContext }) => {
   return (
@@ -13,29 +13,33 @@ const BlogPosts = ({ data, pageContext }) => {
       <div className="grid place-content-center overflow-y-scroll px-1 pb-2">
         <div className="max-w-6xl md:mx-auto h-screen flex flex-col justify-center">
           {data.allMdx.nodes.map((node) => {
-            const { frontmatter } = node
-            const image = getImage(frontmatter.image)
+            const { frontmatter } = node;
+            const image = getImage(frontmatter.image);
             return (
               <div>
                 <article key={node.id}>
-                  <div className="text-pink-800 hover:text-indigo-400 font-cursive text-4xl mt-2 flex flex-col">
+                  <div className="mt-2 flex flex-col">
                     <Link to={`/updates/${node.slug}`}>
-                      {node.frontmatter.title}
-                      <div>
+                      <p className="flex justify-center text-pink-800 hover:text-indigo-400 font-cursive text-3xl ">
+                        {node.frontmatter.title}
+                      </p>
+                      <div className="flex justify-center">
                         <GatsbyImage
                           image={image}
                           alt={frontmatter.imageAlt}
-                          className="w-1/4 mt-2 rounded-xl"
+                          className="w-1/2 mt-2 border-2 border-gray-300 rounded-md"
                         />
                       </div>
                     </Link>
                   </div>
-                  
-                  <p className="mb-4 italic text-gray-700 lg:font-bold lg:text-lg">{node.excerpt}</p>
+
+                  <p className="mb-4 italic text-gray-700 lg:font-bold lg:text-lg">
+                    {node.excerpt}
+                  </p>
                   <hr />
                 </article>
               </div>
-            )
+            );
           })}
           <div>
             <Pagination pageContext={pageContext} />
@@ -44,10 +48,10 @@ const BlogPosts = ({ data, pageContext }) => {
       </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default BlogPosts
+export default BlogPosts;
 
 export const query = graphql`
   query ($skip: Int, $limit: Int) {
@@ -73,4 +77,4 @@ export const query = graphql`
       }
     }
   }
-`
+`;
