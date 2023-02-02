fe9409fe7ab118f0a5051a06944ee21c10989c88
Isaac Tait <isaac@mountaintopcoding.com>
2023-02-02 13:16:53 -0500

    chore:minor tweak 🐸

--- src/components/pager.js
+++ src/components/pager.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from 'react';
+import { Link } from 'gatsby';
 
 const Pagination = ({ pageContext }) => {
-  console.log(pageContext)
-  const { numPages, currentPage } = pageContext
-  const isFirst = currentPage === 1
-  const isLast = currentPage === numPages
+  console.log(pageContext);
+  const { numPages, currentPage } = pageContext;
+  const isFirst = currentPage === 1;
+  const isLast = currentPage === numPages;
   const prevPage =
-    currentPage - 1 === 1 ? '/updates' : (currentPage - 1).toString()
-  const nextPage = (currentPage + 1).toString()
+    currentPage - 1 === 1 ? '/updates' : (currentPage - 1).toString();
+  const nextPage = (currentPage + 1).toString();
 
   return (
     <nav>
-      <div className="flex flex-wrap justify-between">      
+      <div className="flex flex-wrap justify-between">
         {/* Nav Menu*/}
         {Array.from({ length: numPages }, (_, i) => (
-          <div key={`pagination-number${i + 1}`}>
+          <div key={`pagination-number${i + 1}`} className="mb-4">
             <Link
               to={`/updates/${i === 0 ? '' : i + 1}`}
               className="bg-yellow-400 rounded-xl px-2 text-gray-700 hover:text-indigo-400 hover:bg-white"
@@ -26,7 +26,7 @@ const Pagination = ({ pageContext }) => {
         ))}
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Pagination
+export default Pagination;
